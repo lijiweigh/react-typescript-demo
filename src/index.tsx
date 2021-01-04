5d4400b2d913464a87734dc5e75e57fda2d33a0e
lijiwei <lijiwei@fenbi.com>
2021-01-04 10:10:45 +0800

    五子棋

--- src/index.tsx
+++ src/index.tsx
@@ -1,68 +1,122 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import { SquareProps } from './types'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import { SquareProps, SquareState, BoardState } from './types'
 
 class Square extends React.Component<SquareProps> {
+
   render() {
     return (
-      <button className="square" onClick={() => alert('click')}>
+      <button
+        className='square'
+        onClick={() => this.props.onClick()}
+      >
         {this.props.value}
       </button>
-    );
+    )
   }
 }
 
-class Board extends React.Component {
+class Board extends React.Component<{}, BoardState> {
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      squares: new Array(9).fill(null),
+      XIsNext: true
+    }
+  }
+
+  handleClick(i: number) {
+    const squares = this.state.squares.slice()
+    if(calculateWinner(squares) || squares[i]) {
+      return
+    }
+    squares[i] = this.state.XIsNext ? 'X' : 'O'
+    this.setState({
+      squares,
+      XIsNext: !this.state.XIsNext
+    })
+
+  }
+
   renderSquare(i: number) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if(winner) {
+      status = 'Winner is : ' + winner
+    } else {
+      status = 'Next player: ' + (this.state.XIsNext ? 'X' : 'O')
+    }
 
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
+        <div className='status'>{status}</div>
+        <div className='board-row'>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className="board-row">
+        <div className='board-row'>
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className="board-row">
+        <div className='board-row'>
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
       </div>
-    );
+    )
   }
 }
 
 class Game extends React.Component {
   render() {
     return (
-      <div className="game">
-        <div className="game-board">
+      <div className='game'>
+        <div className='game-board'>
           <Board />
         </div>
-        <div className="game-info">
+        <div className='game-info'>
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
-    );
+    )
   }
 }
 
+function calculateWinner(squares: string[] | null[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Game />, document.getElementById('root'))
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,13 @@
 export interface SquareProps {
-  value: number
+  value: string | null
+  onClick: Function
+}
+
+export interface SquareState {
+  value: string
+}
+
+export interface BoardState {
+  squares: string[] | null[]
+  XIsNext: boolean
 }
